8005e19fa7a833a9ae6e1d89eff32001e2b2ce3b
Elise Alix <elise@xmtp.com>
2023-02-09 12:35:15 -0500

    Add a new floating button (#49)
    * add a new floating button
    
    * put behind debug flag until it actually works

---
+++ xmtp-inbox-ios/Views/FloatingButton.swift
@@ -0,0 +1,26 @@
+//
+//  FloatingButton.swift
+//  xmtp-inbox-ios
+//
+//  Created by Elise Alix on 2/8/23.
+//
+
+import SwiftUI
+
+struct FloatingButton: View {
+    var icon: Image
+    var action: () -> Void
+
+    var body: some View {
+        HapticButton(action: {
+            action()
+        }) {
+            icon
+        }
+        .frame(width: 48, height: 48)
+        .background(Color.actionPrimary)
+        .foregroundColor(.actionPrimaryText)
+        .clipShape(Circle())
+        .shadow(radius: 2, x: 0, y: 1)
+    }
+}
