522170208066af78d8a0c8eae3608dd46014c691
Pat Nakajima <patnakajima@gmail.com>
2023-02-27 15:23:26 -0800

    Remove unused extension

--- xmtp-inbox-ios/Extensions/AttachmentExtensions.swift
+++ xmtp-inbox-ios/Extensions/AttachmentExtensions.swift
@@ -7,16 +7,6 @@
 
 import XMTP
 
-extension XMTP.Attachment {
-	enum ContentType {
-		case image
-	}
-
-	var type: ContentType {
-		return .image
-	}
-}
-
 extension XMTP.Attachment: Equatable {
 	public static func == (lhs: Attachment, rhs: Attachment) -> Bool {
 		lhs.mimeType == rhs.mimeType && lhs.filename == rhs.filename && lhs.data == rhs.data
