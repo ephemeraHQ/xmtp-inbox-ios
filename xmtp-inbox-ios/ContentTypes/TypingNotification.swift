b600fcdd9051b572ef151e88d074e2ee655f0b1f
Pat Nakajima <patnakajima@gmail.com>
2023-04-01 19:03:30 -0700

    Move values into parameters

--- xmtp-inbox-ios/ContentTypes/TypingNotification.swift
+++ xmtp-inbox-ios/ContentTypes/TypingNotification.swift
@@ -22,21 +22,49 @@ public struct TypingNotification: Codable {
 }
 
 public struct TypingNotificationCodec: ContentCodec {
+	enum Error: Swift.Error {
+		case missingParameter(String)
+	}
+
 	public init() {}
 
 	public var contentType = ContentTypeTypingNotification
+
+	var dateFormatter: ISO8601DateFormatter {
+		let formatter = ISO8601DateFormatter()
+		formatter.formatOptions.insert(.withFractionalSeconds)
+		return formatter
+	}
 
 	public func encode(content: TypingNotification) throws -> EncodedContent {
 		var encodedContent = EncodedContent()
 
 		encodedContent.type = ContentTypeTypingNotification
-		encodedContent.content = try JSONEncoder().encode(content)
+		encodedContent.parameters["timestamp"] = dateFormatter.string(from: content.timestamp)
+		encodedContent.parameters["typerAddress"] = content.typerAddress
+		encodedContent.parameters["isFinished"] = content.isFinished ? "true" : "false"
+		encodedContent.content = Data()
 
 		return encodedContent
 	}
 
 	public func decode(content: EncodedContent) throws -> TypingNotification {
-		let decoder = JSONDecoder()
-		return try decoder.decode(TypingNotification.self, from: content.content)
+		print("PARM: \(content.parameters)")
+
+		guard let timestampString = content.parameters["timestamp"] else {
+			throw Error.missingParameter("missing timestamp parameter")
+		}
+
+		guard let timestamp = dateFormatter.date(from: timestampString) else {
+			throw Error.missingParameter("invalid timestamp")
+		}
+
+		guard let typerAddress = content.parameters["typerAddress"] else {
+			throw Error.missingParameter("missing typerAddress parameter")
+		}
+
+		let isFinished = content.parameters["isFinished"] == "true"
+
+		return TypingNotification(timestamp: timestamp, typerAddress: typerAddress, isFinished: isFinished)
 	}
 }
